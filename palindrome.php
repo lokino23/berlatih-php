fe4661815731bf215e85220b612bcb9994b8c73a
Ruzaqir <ruzaqir@gmail.com>
2020-07-29 14:58:47 +0700

    enter

--- palindrome.php
+++ palindrome.php
@@ -5,7 +5,7 @@ function palindrome_angka($angka) {
 }
 
 // TEST CASES
-echo palindrome_angka(8) // 9
+echo palindrome_angka(8) ;// 9
 echo palindrome_angka(10); // 11
 echo palindrome_angka(117); // 121
 echo palindrome_angka(175); // 181
