66cb91a782a9568dc5dc11725daf071ad47c8434
Ruzaqir <ruzaqir@gmail.com>
2020-07-30 07:20:48 +0700

    soal

---
+++ ubah-huruf.php
@@ -0,0 +1,12 @@
+function ubah_huruf($string){
+
+}
+
+// TEST CASES
+echo ubah_huruf('wow'); // xpx
+echo ubah_huruf('developer'); // efwfmpqfs
+echo ubah_huruf('laravel'); // mbsbwfm
+echo ubah_huruf('keren'); // lfsfo
+echo ubah_huruf('semangat'); // tfnbohbu
+
+?>
